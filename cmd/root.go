35b5d09711a9be6fa592c09105fce21f6a487648
seemywings <kevin.jayne@icloud.com>
2025-01-30 15:57:31 -0500

    working on geolocation and IP data

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,19 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/seemywingz/go-toolbox"
 	openWeather "github.com/seemywingz/openWeatherGO"
 	"github.com/spf13/cobra"
 )
 
 var verbose bool
+var location string
 
-// rootCmd represents the base command when called without any subcommands
+var lat, long float64
+
 var rootCmd = &cobra.Command{
 	Use:   "wAether",
 	Short: "CLI Weather Information 🌤️",
@@ -17,14 +21,32 @@ var rootCmd = &cobra.Command{
   wAether 🌤️ in your cli.
   All Weather data comes from OpenWeather API.
 `,
-
 	Run: func(cmd *cobra.Command, args []string) {
+		err := openWeather.Init()
+		toolbox.EoE(err, "Error Initializing OpenWeatherMap: ")
+
+		if location != "" {
+			geoData, err := openWeather.GetGeoData(location)
+			toolbox.EoE(err, "Error getting GeoData: ")
+			lat = geoData[0].Lat
+			long = geoData[0].Lon
+		} else {
+			ipInfo, err := toolbox.GetIPInfo()
+			toolbox.EoE(err, "Error getting Location from IP: ")
+			lat = ipInfo.Latitude
+			long = ipInfo.Longitude
+		}
+
+		if verbose {
+			fmt.Println("Location: ", location)
+			fmt.Println("Latitude: ", lat)
+			fmt.Println("Longitude: ", long)
+		}
+
 		openWeather.Now()
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -34,4 +56,5 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(&location, "location", "l", "", "location to get weather information")
 }
